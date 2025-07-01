8bed81eaa571af00d496d069e3acd755b7371841
cubo3d <davisilvaperes3@gmail.com>
2025-07-01 14:24:51 -0300

    modificação no módulo weekdays

--- src/weekdays.py
+++ src/weekdays.py
@@ -1,17 +1,18 @@
 import random
 random_number = random.randint(1, 7)
+# points = 0
 
 def weekdays():
     """
     Escolhe um dia da semana aleatório e pede para traduzir do inglês pro português
-    Detalhe: ele tem uma variável onde armazena todos os ponto. O nome dessa variável é points
     """
 
+
     match random_number:
-        
+
         case 1:
             question1 = input("the translation of sunday is: ")
-            if question1 == "domingo" or question1 == "Domingo":
+            if question1 in ["domingo", "Domingo"]:
                 print("is correct")
                 print("one more point")
                 # points += 1
@@ -23,7 +24,7 @@ def weekdays():
 
         case 2:
             question2 = input("the translation of monday is: ")
-            if question2 == "segunda" or question2 == "segunda-feira" or question2 == "Segunda" or question2 == "Segunda-feira":
+            if question2 in ["segunda", "segunda-feira", "Segunda", "Segunda-feira"]:
                 print("is correct")
                 print("one more point")
                 # points += 1
@@ -35,7 +36,7 @@ def weekdays():
 
         case 3:
             question3 = input("the translation of tuesay is: ")
-            if question3 == "terça" or question3 == "terça-feira" or question3 == "Terça" or question3 == "Terça-feira":
+            if question3 in ["terça", "terça-feira", "Terça", "Terça-feira"]:
                 print("is correct")
                 print("one more point")
                 # points += 1
@@ -47,7 +48,7 @@ def weekdays():
 
         case 4:
             question4 = input("the translation of wednesday is: ")
-            if question4 == "quarta" or question4 == "quarta-feira" or question4 == "Quarta" or question4 == "Quarta-feira":
+            if question4 in ["quarta", "quarta-feira", "Quarta", "Quarta-feira"]:
                 print("is correct")
                 print("one more point")
                 # points += 1
@@ -59,7 +60,7 @@ def weekdays():
 
         case 5:
             question5 = input("the translation of thursday is: ")
-            if question5 == "quinta" or question5 == "quinta-feira" or question5 == "Quinta" or question5 == "Quinta-feira":
+            if question5 in ["quinta", "quinta-feira", "Quinta", "Quinta-feira"]:
                 print("is correct")
                 print("one more point")
                 # points += 1
@@ -71,7 +72,7 @@ def weekdays():
 
         case 6:
             question6 = input("the translation of friday is: ")
-            if question6 == "sexta" or question6 == "sexta-feira" or question6 == "Sexta" or question6 == "Sexta-feira":
+            if question6 in ["sexta", "sexta-feira", "Sexta", "Sexta-feira"]:
                 print("is correct")
                 print("one more point")
                 # points += 1
@@ -83,7 +84,7 @@ def weekdays():
 
         case 7:
             question7 = input("the translation of saturday is: ")
-            if question7 == "sábado" or question7 == "Sábado":
+            if question7 in ["sábado", "Sábado"]:
                 print("is correct")
                 print("one more point")
                 # points += 1
