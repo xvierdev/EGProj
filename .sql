56b7f9f7da01aa8880ce1374c7249fda25ec0c06
anac832 <cn43919@gmail.com>
2025-07-02 19:00:30 -0300

    .sql

---
+++ .sql
@@ -0,0 +1,23 @@
+CREATE TABLE USUARIO ( 
+  id_usuario INT PRIMARY KEY,
+  usuario VARCHAR (100)
+  email VARCHAR (100)
+
+  ); 
+CREATE TABLE PONTUACOES 
+    id_usuario INT PRIMARY KEY,
+    id_usuario INT
+    ponts INT NOT NULL 
+
+);
+
+Insert into usuário values 
+('joao' , 'joao@gmail.com' ),
+
+
+
+Insert into pontuacoes (id_usuario,pontos) values 
+(1,10)
+
+     
+  
