1a02e3025cf33aa815e35fd62152125bc528f63a
fernandodo111 <franciscotrabalho2024@gmail.com>
2025-07-04 22:43:08 -0300

    Update pronouns.py

--- src/modules/pronouns.py
+++ src/modules/pronouns.py
@@ -1,86 +1,69 @@
 import random
 
-
 def pronouns_test():
+  response_quest1 = ["ele"]
+  response_quest2 = ["ela"]
+  response_quest3 = ["eles", "elas"]
+  response_quest4 = ["voce","voçe"]
+  response_quest5 = ["eu"]
+  random_pronoun = random.randint(1, 4)
+  match random_pronoun:
+   
+    case 1:
 
-    random_pronoun = random.randint(1, 4)
-    match random_pronoun:
+     quest1 = input("A tradução de 'he' é: ")
+    
+     if quest1.lower() == response_quest1[0].lower():
+      print('Correct!')
+    
+     else:
+       print('incorrect')
 
-        case 1:
-            quest1 = input("A tradução de 'he' é: ")
+    case 2:
+     
+      quest2 = input("A tradução de 'she' é: ")
+      
+      if quest2.lower() == response_quest2[0].lower():
+        print('Correct')
+      
+      else: 
+        print('Incorrect!')
 
-            if quest1 == 'ele':
-                print('Correct!')
+    case 3:
+    
+      quest3 = input("A tradução de 'they' é: ")
+    
+      if quest3.lower() == response_quest3[0].lower():
+        print('Correct!')
 
-            elif quest1 == 'Ele':
-                print('Correct!')
+      elif quest3.lower() == response_quest3[1].lower():
+        print('Correct!')
 
-            else:
-                print('incorrect')
+      else: 
+        print('incorrect!')
 
-        case 2:
+    case 4:
+    
+     quest4 = input("A tradução de 'you' é: ")
+    
+     if quest4.lower() == response_quest4[0].lower():
+      print('Correct!')
 
-            quest2 = input("A tradução de 'she' é: ")
+     elif quest4.lower() == response_quest4[1].lower():
+      print('Correct!')
 
-            if quest2 == 'ela':
-                print('Correct')
+     else: 
+      print('Incorrect!')
+    
+    case 5:
+     
+     quest5 =  input("A tradução de 'I' é: ")
+     
+     if quest5.lower() == response_quest5[0].lower():
+       print('Correct!')
 
-            elif quest2 == 'Ela':
-                print('Correct')
-
-            else:
-                print('Incorrect!')
-
-        case 3:
-
-            quest3 = input("A tradução de 'they' é: ")
-
-            if quest3 == 'eles':
-                print('Correct!')
-
-            elif quest3 == 'Eles':
-                print('Correct!')
-
-            elif quest3 == 'elas':
-                print('Correct!')
-
-            elif quest3 == 'Elas':
-                print('Correct!')
-
-            else:
-                print('incorrect!')
-
-        case 4:
-
-            quest4 = input("A tradução de 'you' é: ")
-
-            if quest4 == 'Você':
-                print('Correct!')
-
-            elif quest4 == 'você':
-                print('Correct!')
-
-            elif quest4 == 'Voce':
-                print('Correct!')
-
-            elif quest4 == 'voce':
-                print('Correct!')
-
-            else:
-                print('Incorrect!')
-
-        case 5:
-
-            quest5 = input("A tradução de 'I' é: ")
-
-            if quest5 == 'Eu':
-                print('Correct!')
-
-            elif quest5 == 'eu':
-                print('Correct!')
-
-            else:
-                print('Incorrect!')
-
-
+     else: 
+       print('Incorrect!')
+#apagar uso para teste
 pronouns_test()
+
