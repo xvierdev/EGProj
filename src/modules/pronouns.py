4031eab3d05e023b6858fb15780f419148aa2919
Wesley Xavier <wesley.xvier@gmail.com>
2025-07-06 18:06:54 -0300

    :recycle: refactor: refatoração de pronouns.py

--- src/modules/pronouns.py
+++ src/modules/pronouns.py
@@ -1,71 +1,29 @@
-import random
+from random import choice
 
 
 def pronouns_test():
 
-    response_quest1 = ["ele"]
-    response_quest2 = ["ela"]
-    response_quest3 = ["eles", "elas"]
-    response_quest4 = ["voce", "voçe"]
-    response_quest5 = ["eu"]
-    random_pronoun = random.randint(1, 4)
-    match random_pronoun:
-
-        case 1:
-
-            quest1 = input("A tradução de 'he' é: ")
-
-            if quest1.lower() == response_quest1[0].lower():
-                print('Correct!')
-
-            else:
-                print('incorrect')
-
-        case 2:
-
-            quest2 = input("A tradução de 'she' é: ")
-
-            if quest2.lower() == response_quest2[0].lower():
-                print('Correct')
-
-            else:
-                print('Incorrect!')
-
-        case 3:
-
-            quest3 = input("A tradução de 'they' é: ")
-
-            if quest3.lower() == response_quest3[0].lower():
-                print('Correct!')
-
-            elif quest3.lower() == response_quest3[1].lower():
-                print('Correct!')
-
-            else:
-                print('incorrect!')
-
-        case 4:
-
-            quest4 = input("A tradução de 'you' é: ")
-
-            if quest4.lower() == response_quest4[0].lower():
-                print('Correct!')
-
-            elif quest4.lower() == response_quest4[1].lower():
-                print('Correct!')
-
-            else:
-                print('Incorrect!')
-
-        case 5:
-
-            quest5 = input("A tradução de 'I' é: ")
-
-            if quest5.lower() == response_quest5[0].lower():
-                print('Correct!')
-
-            else:
-                print('Incorrect!')
+    pronouns = {'I': 'eu',
+                'you': ('você', 'vocês'),
+                'he': 'ele',
+                'she': 'ela',
+                'it': ('isso', 'isto', 'aquilo'),
+                'we': 'nós',
+                'they': ('eles', 'elas'),
+                'someone': 'alguém',
+                'something': 'algo',
+                'everyone': ('todos', 'todas'),
+                'everything': 'tudo',
+                'nobody': 'ninguém',
+                'nothing': 'nada',
+                'any': 'qualquer',
+                'some': ('algum', 'alguma')}
+    pronoun = choice(list(pronouns.keys()))
+    answer = input(f'A tradução de \'{pronoun}\' é: ').strip().lower()
+    if answer in pronouns[pronoun]:
+        print('Correct')
+    else:
+        print('Incorrect')
 
 
 if __name__ == '__main__':
@@ -95,7 +53,7 @@ if __name__ == '__main__':
         'you (plural, informal, obj)': 'vos/lhes',  # ou 'vocês'
         'them': 'os/as/lhes',
 
-        # Pronomes Possessivos (alguns exemplos, pois variam em gênero e número)
+        # Pronomes Possessivos (variam em gênero e número)
         'my': 'meu/minha',
         'your (singular, informal)': 'teu/tua',
         'his': 'dele/dela',
