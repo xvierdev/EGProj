3db27ee1a5afd09673f921285441a6094a67b52d
fernandodo111 <franciscotrabalho2024@gmail.com>
2025-07-08 22:57:28 -0300

    Update color.sql
    adicionado  opções de adicionar as cores a database e de apagar a databse

--- src/modules/color.sql
+++ src/modules/color.sql
@@ -1,6 +1,52 @@
-CREATE TABLE
-    IF NOT EXISTS database_color (
-        id INTEGER PRIMARY KEY auto_increment,
-        color_english VARCHAR(50) UNIQUE NOT NULL,
-        color_portuguese VARCHAR(50) NOT NULL
-    )
+import random
+from database_color import creat_dataase_color,boot,completion,cursor
+points = 9
+
+def colors():
+    global points
+    while True:
+        creat_dataase_color()
+        completion()
+        boot()
+        print("╔═════════════════════════════════╗")
+        print("║          jogo das cores         ║")
+        print("╚═════════════════════════════════╝")
+        print("(q)sair,(1)iniciar, (2)pontuação")
+        menu = input("selecione um opção :")
+        if menu == "q":
+            break
+
+        elif menu == "1":
+            print("How does the game work?\n"
+                "a window will appear in English and you must type "
+                "your translation into Portuguese\nthe game will start")
+            while True:
+                cursor.execute(
+                    "SELECT color_english,"
+                    "color_Portuguese FROM database_color")
+                color_r = cursor.fetchall()
+                random_colors = random.choice(color_r)
+                color_english, color_portuguese = random_colors
+                print("qual a tradução dessa cor:", color_english)
+                response = str(input(">"))
+                if response.lower().strip() == color_portuguese.lower():
+                    print("correto +1 ponto")
+                    points += 1
+                    print("pontos :", points)
+                    if points == 10:
+                        print("!parabems! voçe completou esse quiz ")
+                        break
+                else:
+                    points -= 1
+                    print('Resposta incorreta!')
+                    print(f'A resposta era: {color_portuguese}')
+
+        elif menu == "2":
+            print("How does it work?")
+            print("Every time you get a question right you earn a point")
+            print("Every time you get a question wrong you lose a point")
+            print("Current score is:", points)
+
+
+if __name__ == "__main__":
+    colors()
