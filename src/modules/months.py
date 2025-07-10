1ebddec5f703ac44dd6a75ad821140d211c98ecb
Wesley Xavier <wesley.xvier@gmail.com>
2025-07-10 18:26:59 -0300

    :sparkles: feat: implementado while true no módulo months.py

--- src/modules/months.py
+++ src/modules/months.py
@@ -1,37 +1,44 @@
-import random
+from random import choice
+import utils
 
-# escolhe um mês do ano aleatório e traduz do inglês pro português 
+# escolhe um mês do ano aleatório e traduz do inglês pro português
 
 Months = {
-     'January': 'janeiro',
-     'February': 'fevereiro',
-     'March':  'março',
-     'April': 'Abril',
-     'May': 'maio',
-     'June':'junho',
-     'July':'julho',
-     'August': 'agosto',
-     'September':'setembro',
-     'October':'outubro',
-     'November' : 'novembro',
-     'December' : 'dezembro',
+    'January': 'janeiro',
+    'February': 'fevereiro',
+    'March':  'março',
+    'April': 'Abril',
+    'May': 'maio',
+    'June': 'junho',
+    'July': 'julho',
+    'August': 'agosto',
+    'September': 'setembro',
+    'October': 'outubro',
+    'November': 'novembro',
+    'December': 'dezembro',
 }
 
-def start():
-    english_month = random.choice(list(Months.keys()))
-    print(f"the translation of : '{english_month}' to Portuguese is:")
-    user_answer = input("your answer: ").strip().lower()
 
-    if check_answer(english_month, user_answer):
-        print("t: the answer is correct!")
-        print("one more point!")
-    else:
-        print("the answer is incorrect.")
-        print("minus one point.")
+def start():
+    while True:
+        english_month = choice(list(Months.keys()))
+        print(f"the translation of : '{english_month}' to Portuguese is:")
+        user_answer = input("your answer: ").strip().lower()
+
+        if check_answer(english_month, user_answer):
+            print("t: the answer is correct!")
+            print("one more point!")
+        else:
+            print("the answer is incorrect.")
+            print("minus one point.")
+        if not utils.ask_play_again():
+            break
+
 
 def check_answer(english_month, user_answer):
     correct_translation = Months[english_month]
     return user_answer == correct_translation
+
 
 if __name__ == '__main__':
     start()
