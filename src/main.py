d33b4e8cd20a2f8819f3ce40784a42161895b076
Wesley Xavier <wesley.xvier@gmail.com>
2025-07-14 18:24:19 -0300

    :sparkles: feat: adicição da integração base com flask.

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import modules.func_test
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('base.html',
+                           content=modules.func_test.func_test())
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ src/modules/func_test.py
@@ -0,0 +1,6 @@
+def func_test():
+    """
+    This function is a placeholder for testing purposes.
+    It currently does nothing but can be expanded in the future.
+    """
+    return 'This is a test function.'
