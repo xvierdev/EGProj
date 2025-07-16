be154be4304aa4e949ff3f300a1b172d922a623a
Wesley Xavier <wesley.xvier@gmail.com>
2025-07-16 20:58:14 -0300

    :books: docs: aplicação parcial de pep8.

--- src/modules/colors.py
+++ src/modules/colors.py
@@ -1,9 +1,15 @@
 import random
-from modules.database_color import creat_database_color, boot, completion, get_cursor
+from modules.database_color import (
+    creat_database_color,
+    boot,
+    completion,
+    get_cursor,
+)
 from modules.utils import ask_play_again
 
+
 def level(points):
-    global levels 
+    global levels
     levels = 0
     if points >= 0:
         levels = 1
@@ -12,25 +18,25 @@ def level(points):
     if points >= 20:
         levels = 3
 
+
 def random_simple_color():
     global simples_color_english, simple_color_portuguese
     get_cursor().execute(
-    "SELECT simple_color_english ,"
-    "simple_color_Portuguese FROM database_color")
+        "SELECT simple_color_english ,"
+        "simple_color_Portuguese FROM database_color")
     color_r = get_cursor().fetchall()
     simple_random_colors = random.choice(color_r)
     simples_color_english, simple_color_portuguese = simple_random_colors
 
+
 def random_advanced_color():
     global advanced_color_english, advanced_color_portuguese
     get_cursor().execute(
-    "SELECT advanced_color_english ,"
-    "advanced_color_Portuguese FROM database_color")
+        "SELECT advanced_color_english ,"
+        "advanced_color_Portuguese FROM database_color")
     color_r = get_cursor().fetchall()
     advaanced_random_colors = random.choice(color_r)
     advanced_color_english, advanced_color_portuguese = advaanced_random_colors
-
-
 
 
 def colors():
@@ -45,18 +51,19 @@ def colors():
         boot()
         level(points)
         print("╔═════════════════════════════════╗")
-        print("║          jogo das cores         ║")
+        print("║            color game           ║")
         print("╚═════════════════════════════════╝")
-        print("(q) quit, (1) start game,(2) tutorial")
+        print("(q) quit, (1) start game, (2) tutorial")
         menu = input("Choose your option: ")
         if menu == "q":
-            break 
+            break
 
         elif menu == "1":
             if levels == 1:
                 while True:
                     random_simple_color()
-                    print("What is the translation of this color:", simples_color_english)
+                    print("What is the translation of this color:",
+                          simples_color_english)
                     response = str(input("> "))
                     if response.lower().strip() == simple_color_portuguese.lower():
                         print("Correct! +1 point.")
@@ -71,20 +78,18 @@ def colors():
                     else:
                         points -= 1
                         print('Incorrect answer!')
-                        print(f'Correct answer: {simple_color_portuguese}') 
+                        print(f'Correct answer: {simple_color_portuguese}')
 
             elif levels == 2:
                 while True:
                     random_advanced_color()
-                    print("What is the translation of this color:", advanced_color_english)
+                    print("What is the translation of this color:",
+                          advanced_color_english)
                     response = str(input("> "))
-                    if response.lower().strip() == advanced_color_portuguese.lower():
+                    if response.strip().lower() == advanced_color_portuguese:
                         print("Correct! +1 point.")
                         points += 1
                         print("Current points:", points)
-                        if not ask_play_again():
-                            break
-
                         if points == 20:
                             print("Congratulations! You've level up!")
                             break
@@ -92,30 +97,35 @@ def colors():
                         points -= 1
                         print('Incorrect answer!')
                         print(f'Correct answer: {advanced_color_portuguese}')
+                    if not ask_play_again():
+                        break
 
             elif levels == 3:
                 while True:
-                        random_simple_color(),random_advanced_color()
-                        print("What is the translation of this color:", simples_color_english, advanced_color_english)
-                        response_simple = str(input("first color> "))
-                        response_advanced = str(input("second color> "))
-                        if response_simple.lower() == simple_color_portuguese.lower():
-                            if response_advanced.lower() == advanced_color_portuguese.lower():
-                                print("Correct! +1 point.")
-                                points += 1
-                                print("Current points:", points)
-                                if  points == 30:
-                                    print("Congratulations! You've level up!")
-                                    break
-                                if not ask_play_again():
-                                        break
-                                    
-                        if response_simple.lower() != simple_color_portuguese.lower() or response_advanced.lower() != advanced_color_portuguese.lower():
-                            points -= 1
-                            print('Incorrect answer!')
-                            print(f'Correct answer: {simple_color_portuguese}, {advanced_color_portuguese}')
+                    random_simple_color()
+                    random_advanced_color()
+                    print("What is the translation of this color:",
+                          simples_color_english, advanced_color_english)
+                    response_simple = str(input("first color> "))
+                    response_advanced = str(input("second color> "))
+                    if response_simple.strip().lower() == simple_color_portuguese:
+                        if response_advanced.strip().lower() == advanced_color_portuguese:
+                            print("Correct! +1 point.")
+                            points += 1
+                            print("Current points:", points)
+                            if points == 30:
+                                print("Congratulations! You've level up!")
+                                break
 
-                            
+                    if response_simple.lower() != simple_color_portuguese.lower() or response_advanced.lower() != advanced_color_portuguese.lower():
+                        points -= 1
+                        print('Incorrect answer!')
+                        print(
+                            f'Correct answer: {simple_color_portuguese}, {advanced_color_portuguese}')
+
+                    if not ask_play_again():
+                        break
+
         elif menu == "2":
             while True:
                 print("How does it work?")
@@ -125,9 +135,10 @@ def colors():
                 print("start with level 1 simple colors")
                 print("10 points level 2 advanced colors")
                 print("20 points Level 3 Simple and Advanced colors")
-                print("Current score:", points,"your level:", levels)
+                print("Current score:", points, "your level:", levels)
                 if not ask_play_again():
                     break
+
 
 if __name__ == "__main__":
     colors()
--- src/modules/database_color.py
+++ src/modules/database_color.py
@@ -2,6 +2,7 @@ import sqlite3
 conexao = sqlite3.connect('database_color.db')
 cursor = conexao.cursor()
 
+
 def get_cursor():
     return cursor
 
@@ -15,28 +16,29 @@ def creat_database_color():
         simple_color_Portuguese TEXT,
         advanced_color_english TEXT,
         advanced_color_Portuguese TEX
-        )""")                  
+        )""")
     conexao.commit()
+
 
 def boot():
     simple_colors_english = [
-        "Black", "White", "Red", "Blue", "Yellow",
-        "Green", "Orange", "Pink", "Purple", "Brown", "Gray"
-]
+        "black", "white", "red", "blue", "yellow",
+        "green", "orange", "pink", "purple", "brown", "gray"
+    ]
     simple_colors_portuguese = [
-    "Preto", "Branco", "Vermelho", "Azul", "Amarelo",
-    "Verde", "Laranja", "Rosa", "Roxo", "Marrom", "Cinza"
-]
+        "preto", "branco", "vermelho", "azul", "amarelo",
+        "verde", "laranja", "rosa", "roxo", "marrom", "cinza"
+    ]
     advanced_colors_english = [
-    "Cyan", "Magenta", "Turquoise", "Violet", "Beige",
-    "Gold", "Silver", "Burgundy", "Salmon", "Lavender", "Olive"
-]
+        "cyan", "magenta", "turquoise", "violet", "beige",
+        "gold", "silver", "burgundy", "salmon", "lavender", "olive"
+    ]
     advanced_colors_portuguese = [
-    "Ciano", "Magenta", "Turquesa", "Violeta", "Bege",
-    "Dourado", "Prateado", "Bordô", "Salmão", "Lavanda", "Oliva"
-]
-    for simple_colors_e, simple_colors_p , advanced_colors_e, advanced_colors_p in zip(simple_colors_english, simple_colors_portuguese,
-        advanced_colors_english, advanced_colors_portuguese):
+        "ciano", "magenta", "turquesa", "violeta", "bege",
+        "dourado", "prateado", "bordô", "salmão", "lavanda", "oliva"
+    ]
+    for simple_colors_e, simple_colors_p, advanced_colors_e, advanced_colors_p in zip(simple_colors_english, simple_colors_portuguese,
+                                                                                      advanced_colors_english, advanced_colors_portuguese):
         cursor.execute(
             '''INSERT INTO database_color (simple_color_english, simple_color_Portuguese, 
             advanced_color_english,advanced_color_portuguese)
@@ -47,5 +49,3 @@ def boot():
 def completion():
     cursor.execute("DELETE FROM DATABASE_COLOR WHERE id > 0")
     conexao.commit()
-
-   
