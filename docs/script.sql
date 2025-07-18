3849a8b074d85d16e899b082f48d505f144c5f39
Wesley Xavier <wesley.xvier@gmail.com>
2025-07-18 17:48:04 -0300

    :scroll: docs: atualização de script.sql

--- docs/script.sql
+++ docs/script.sql
@@ -2,12 +2,33 @@ CREATE DATABASE EnglishTerminal;
 
 USE EnglishTerminal;
 
-CREATE TABLE User (
+CREATE TABLE user (
   id INT PRIMARY KEY auto_increment, -- Unique identifier for each user
   name VARCHAR(100) NOT NULL, -- User's name
   login VARCHAR(100) NOT NULL UNIQUE, -- Unique login for the user
   password VARCHAR(100) NOT NULL,  -- User's password
   createtime TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Timestamp of when the user was created
+);
+
+CREATE TABLE module (
+  module_id INT PRIMARY KEY auto_increment,
+  record_user_id INT,
+  description TEXT,
+  count_total_acess INT NOT NULL DEFAULT 0,
+  module_name TEXT NOT NULL,
+  record_value INT DEFAULT 0,
+  record_datetime TIMESTAMP,
+  FOREIGN KEY record_user_id REFERENCES user (id)
+);
+
+CREATE TABLE score (
+  user_id INT NOT NULL,
+  module_id INT NOT NULL,
+  total_score INT NOT NULL DEFAULT 0,
+  count_access INT NOT NULL DEFAULT 1,
+  record INT DEFAULT 0,
+  FOREIGN KEY user_id REFERENCES user (id),
+  FOREIGN KEY module_id REFERENCES module (id)
 );
 
 INSERT INTO
