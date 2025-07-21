1cf828b85602b34074d27956481d71a7d229ae12
Wesley Xavier <wesley.xvier@gmail.com>
2025-07-21 03:04:19 -0300

    :sparkles: feat: menu de login.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,11 @@
+from typing import Optional
 from modules import weekdays, number, colors, geomtest, pronouns, months
+from services.user_service import authenticate_user, create_user, guest_user
+from getpass import getpass
 from colorama import Fore, Back, Style, init
 from core import core
+from models.user import User
+from os import system
 
 init(autoreset=True)
 
@@ -11,20 +16,51 @@ funcs = {
     "4": "Geometry",
     "5": "Pronouns",
     "6": "Months",
-    "q": "Quit"
+    "q": "Quit",
 }
 
 
-def main_menu():
+def user_menu() -> Optional[User]:
+    user = None
+    print("Welcome to the English Console App!")
+    while True:
+        print("Choose your option:")
+        print("1 - Login | 2 - Create new account | 3 - Guest user | 4 - exit")
+        option = input("> ")
+        system('cls') # TODO: view this option for portability
+        if option == "1":
+            login = input("user: ")
+            password = getpass("password: ")
+            user = authenticate_user(login, password)
+        elif option == "2":
+            name = input("Your name: ")
+            login = input("User login: ")
+            password = getpass("Your password: ")
+            user = create_user(name, login, password)
+        elif option == "3":
+            user = guest_user()
+        elif option == "4":
+            exit()
+        if user is not None:
+            system('cls')
+            return user
+
+
+def main_menu(user: User):
     """Display a simple menu to the user and handle their choices.F
     This function provides a menu with options to show weekdays or
     exit the program.
     It runs in a loop until the user chooses to exit.
     """
-    print(Fore.YELLOW + "Welcome to the English App!")
-    print(f"Press {Fore.CYAN} Ctrl + C",
-          f"{Fore.RESET} to {Style.BRIGHT} exit",
-          f"{Style.RESET_ALL} at any time.")
+    if user is None:
+        print('User is not valid.')
+        exit()
+    print(Fore.YELLOW + f"{user.user_name.capitalize()}, welcome to the English App!")
+    print(
+        f"Press {Fore.CYAN} Ctrl + C",
+        f"{Fore.RESET} to {Style.BRIGHT} exit",
+        f"{Style.RESET_ALL} at any time.",
+    )
     try:
         while True:
             print()
@@ -35,26 +71,29 @@ def main_menu():
             choice = input("Enter your choice: ")
 
             match choice:
-                case '1':
+                case "1":
                     core(weekdays.WeekdaysTest())
-                case '2':
+                case "2":
                     core(number.NumberTest())
-                case '3':
+                case "3":
                     colors.colors()
-                case '4':
+                case "4":
                     core(geomtest.GeometryTest())
-                case '5':
+                case "5":
                     core(pronouns.PronounsTest())
-                case '6':
+                case "6":
                     core(months.MonthsTest())
-                case 'q':
-                    print(f"{Fore.YELLOW}{Back.BLACK}Goodbye ...")
+                case "q":
+                    print(f"{Fore.YELLOW}Goodbye ...")
                     break
                 case _:
+                    system('cls')
                     print("Invalid choice. Please try again.")
     except KeyboardInterrupt:
         print("\nExiting the program.")
 
 
 if __name__ == "__main__":
-    main_menu()
+    user = user_menu()
+    if user is not None:
+        main_menu(user)
