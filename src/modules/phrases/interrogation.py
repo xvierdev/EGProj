f781fd085da5c07d59fbfaa6364441ab5a82e4f0
Wesley Xavier <wesley.xvier@gmail.com>
2025-07-25 00:21:43 -0300

    :recycle: refactor: refatoração partical de código.

--- src/modules/phrases/interrogation.py
+++ src/modules/phrases/interrogation.py
@@ -4,15 +4,15 @@ welcome_msg = '\
 =========================\n\
 |interrogative sentences|\n\
 =========================\n\
-\
+\n\
 Antes de começarmos a passar as frases afirmativas para interrogativas\
-vai aqui um exemplo!\n \
+vai aqui um exemplo!\n\
 Afirmativa: \'John will make the dinner.\'\n\
 Interrogativa: \'Will John make the dinner?\'\n\
-\
+\n\
 Afirmativa: \'Mary got a book.\'\n\
 Interrogativa: \'Has Mary got a book?\'\n\
-\
+\n\
 ' + '=' * 138 + '\n\
 Nesse caso, percebemos que, na afirmativa temos: sujeito + verbo principal \
 e na interrogativa: verbo auxiliar + sujeito + verbo principal.\n\
@@ -32,194 +32,205 @@ def interrogquest():
         'Does John watches Breaking Bad?'
     )
 
-    print(p1.ask_question())
-
-    pergunta1 = (
-        input(
-            '"John watches Breaking Bad"\n\nA) Watches John Breaking Bad?\nB) Breaking Bad John watches?\nC) Does John watch Breaking Bad?\nD) Does John watches Breaking Bad?\n\nResposta: '
-        )
-        .strip()
-        .upper()
+    p2 = Question(
+        'Marco lives in Japan',
+        'Does Marco live in Japan?',
+        'Lives Marco in Japan?',
+        'Do Marco live in Japan?',
+        'Marco live in Japan?'
     )
 
-    if pergunta1 == "C":
+    print(p1)
+    answer = input('> ').strip().upper()[0]
+    if p1.check_answer(answer):
         print("Correct!")
         points += 1
-
     else:
         print("Incorrect!")
 
-    print("------------------------------------------------")
+    print('-' * 48)
 
-    pergunta2 = (
-        input(
-            '\n"Marco lives in Japan"\n\nA) Lives Marco in Japan?\nB) Does Marco live in Japan?\nC) Do Marco live in Japan?\nD) Marco live in Japan?\n\nReposta: '
-        )
-        .strip()
-        .upper()
-    )
-
-    if pergunta2 == "B":
+    print(p2)
+    answer = input('> ').strip().upper()[0]
+    if p2.check_answer(answer):
         print("Correct!")
         points += 1
-
     else:
         print("Incorrect!")
 
+    print('=' * 78)
+    print()
     print(
-        "=============================================================================="
-    )
-    print(
-        "\nAgora é hora de escrever! Reescreva as frases afirmativas para interrogativas!"
+        'Agora é hora de escrever! Reescreva as ',
+        'frases afirmativas para interrogativas!'
     )
 
-    # Alternei entre upper e lower nas duas frases porque estava testando o comando
-
-    phconstructor1 = input(
-        '\n1) "Mariene plays volleyball" \n\nReescreva: ').upper()
+    print()
+    print('1) "Mariene plays volleyball"')
+    print()
+    phconstructor1 = input('Reescreva: ').upper()
 
     if phconstructor1 == "DOES MARIENE PLAY VOLLEYBALL?":
         print("-> Perfect! Your answer is correct!")
 
     else:
-        print(
-            " -> The answer is incorrect!\nCorrect answer: Does Mariene play volleyball?"
-        )
+        print(' -> The answer is incorrect!')
+        print('Correct answer: Does Mariene play volleyball?')
 
     print("------------------------------------------------")
-
-    phconstructor2 = input(
-        '\n\n2) "Sam likes to play videogames"\n\nReescreva: '
-    ).lower()
+    print()
+    print('2) "Sam likes to play videogames"')
+    print()
+    phconstructor2 = input('Reescreva: ').lower()
 
     if phconstructor2 == "does sam like to play videogames?":
         print("-> Correct!!")
         points += 1
 
     else:
-        print(
-            " -> Your answer is incorrect!\nCorrect answer: Does Sam like to play videogames?"
-        )
+        print(' -> Your answer is incorrect!')
+        print('Correct answer: Does Sam like to play videogames?')
 
     print("------------------------------------------------")
 
-    phconstructor3 = input(
-        '\n\n3) "Olivia will travel to Italy"\n\nReescreva: '
-    ).lower()
+    print()
+    print('3) "Olivia will travel to Italy"')
+    print()
+
+    phconstructor3 = input('Reescreva: ').lower()
 
     if phconstructor3 == "will olivia travel to italy?":
         print("-> You answer is right!")
         points += 1
 
     else:
-        print(" -> Your answer is wrong!\nCorrect answer: Will Olivia travel to Italy?")
+        print(' -> Your answer is wrong!')
+        print('Correct answer: Will Olivia travel to Italy?')
 
     print("------------------------------------------------")
 
-    phconstructor4 = input(
-        '\n\n4) "Josh eats an apple every day"\n\nReescreva: '
-    ).lower()
+    print()
+    print('4) "Josh eats an apple every day"')
+    print()
+
+    phconstructor4 = input('Reescreva: ').lower()
 
     if phconstructor4 == "does josh eat an apple every day?":
         print("-> Your answer is... CORRECT!")
         points += 1
 
     else:
-        print(
-            " -> Your answer is... incorrect! :( \nCorrect answer: Does Josh eat an apple every day?"
-        )
+        print(' -> Your answer is... incorrect! :(')
+        print('Correct answer: Does Josh eat an apple every day?')
 
     print("------------------------------------------------")
 
-    phconstructor5 = input(
-        '\n\n5) "Tyler wants to travel\n\nReescreva: ').lower()
+    print()
+    print('5) "Tyler wants to travel"')
+    print()
+
+    phconstructor5 = input('Reescreva: ').lower()
 
     if phconstructor5 == "does tyler want to travel?":
         print("-> Your answer is correct!! CONGRATS!!")
         points += 1
 
     else:
-        print(
-            " -> Your answer is WRONG >:(\nCorrect answer: Does Tyler want to travel?"
-        )
+        print(' -> Your answer is WRONG >:(')
+        print('Correct answer: Does Tyler want to travel?')
 
     print("------------------------------------------------")
 
-    phconstructor6 = input(
-        '\n\n6) "Edwin is making an animatronic"\n\nReescreva: '
-    ).lower()
+    print()
+    print('6) "Edwin is making an animatronic"')
+    print()
+
+    phconstructor6 = input('Reescreva: ').lower()
 
     if phconstructor6 == "is edwin making an animatronic?":
         print("-> Correct!")
         points += 1
 
     else:
-        print(
-            "-> Your answer is incorrect... ;(\nCorrect answer: Is Edwin making an animatronic?"
-        )
+        print('-> Your answer is incorrect... ;(')
+        print('Correct answer: Is Edwin making an animatronic?')
 
     print("------------------------------------------------")
 
-    phconstructor7 = input(
-        '\n\n7) "Kendrick is cooking\n\nReescreva: ').lower()
+    print()
+    print('7) "Kendrick is cooking"')
+    print()
+
+    phconstructor7 = input('Reescreva: ').lower()
 
     if phconstructor7 == "is kendrick cooking?":
         print("-> Your answer is right! :)")
         points += 1
 
     else:
-        print(" -> Wrong!\nCorrect answer: Is kendrick cooking? ")
+        print(' -> Wrong!')
+        print('Correct answer: Is kendrick cooking? ')
 
     print("------------------------------------------------")
 
-    phconstructor8 = input(
-        "\n\n8) He's taking his dog for a walk\n\nReescreva: "
-    ).lower()
+    print()
+    print('8) "He\'s taking his dog for a walk"')
+    print()
+
+    phconstructor8 = input("Reescreva: ").lower()
 
     if phconstructor8 == "is he going to take his dog for a walk?":
         print("-> Correct!")
         points += 1
 
     else:
-        print(" -> Incorrect!\nCorrect answer: Is he going to take his dog for a walk?")
+        print(' -> Incorrect!')
+        print('Correct answer: Is he going to take his dog for a walk?')
 
     print("------------------------------------------------")
 
-    phconstructor9 = input(
-        '\n\n9) "They are going to build a mall"\n\nReescreva: '
-    ).lower()
+    print()
+    print('9) "They are going to build a mall"')
+    print()
+
+    phconstructor9 = input('Reescreva: ').lower()
 
     if phconstructor9 == "are they going to build a mall?":
         print("-> Your answer is right!!! :))))))))")
         points += 1
 
     else:
-        print(" -> Wrong...\nCorrect answer: Are they going to build a mall?")
+        print(' -> Wrong...')
+        print('Correct answer: Are they going to build a mall?')
 
     print("------------------------------------------------")
 
-    phconstructor10 = input(
-        '\n\n10) "Chris and Joseph are going to school"\n\nReescreva: '
-    ).lower()
+    print()
+    print('10) "Chris and Joseph are going to school"')
+    print()
+
+    phconstructor10 = input('Reescreva: ').lower()
 
     if phconstructor10 == "are chris and joseph going to school?":
         print("-> CORRECT ANSWER!!!")
         points += 1
 
     else:
-        print(
-            "Incorrect.... ;(\nCorrect answer: Are Chris and Joseph going to school?")
+        print('Incorrect.... ;(')
+        print('Correct answer: Are Chris and Joseph going to school?')
 
     print("-----------------------------------------------------")
 
-    print("                  Resultado: \n")
+    print()
+    print("Resultado:")
+    print()
 
     if points >= 6:
-        print(f"Parabéns!! você ganhou {points} pontos :)")
+        print(f'Parabéns!! você ganhou {points} pontos :)')
 
     elif points < 6:
-        print(
-            f"Continue estudando e a melhora virá... você ganhou {points} pontos.")
+        print(f"Continue estudando e a melhora virá ... \
+              você ganhou {points} pontos.")
 
     print("==============================================================")
 
--- src/modules/phrases/question.py
+++ src/modules/phrases/question.py
@@ -1,29 +1,41 @@
+from random import shuffle
+from string import ascii_uppercase
+_LETTERS = ascii_uppercase
+
+
 class Question:
-    def __init__(self, phrase, correct_answer, *other):
+    def __init__(self, phrase: str, correct_answer: str,
+                 *incorrect_answers: str):
         self.question_phrase = phrase
-        self.correct_aswer = correct_answer
-        self.alternatives = other
+        self.correct_answer = correct_answer
+        self.alternatives = incorrect_answers
+        self.all_alternatives: dict[str, str] = {}
+        self.correct_alternative = ''
+        alternatives = list(incorrect_answers)
+        alternatives.append(self.correct_answer)
+        if len(alternatives) > len(_LETTERS):
+            raise ValueError('O número de argumentos excedeu o limite.')
+        shuffle(alternatives)
+        for i in range(len(alternatives)):
+            self.all_alternatives[_LETTERS[i]] = alternatives[i]
+            if alternatives[i] == self.correct_answer:
+                self.correct_alternative = _LETTERS[i]
 
     def __repr__(self) -> str:
         return (
             f"Question(question_phrase='{self.question_phrase}', "
-            f"correct_aswer='{self.correct_aswer}', "
+            f"correct_answer='{self.correct_answer}', "
             f"alternatives={self.alternatives})"
         )
 
     def __str__(self) -> str:
+        alternative_lines = []
+        for key, value in self.all_alternatives.items():
+            alternative_lines.append(f'{key}) {value}')
+        alternatives_str = '\n'.join(alternative_lines)
         return (
-            f"Question: '{self.question_phrase}'\n"
-            f"Correct Answer: '{self.correct_aswer}'\n"
-            f"Alternatives: {self.alternatives}"
+            f"Question: '{self.question_phrase}'\n{alternatives_str}"
         )
 
-    def ask_question(self):
-        from random import shuffle
-        letters = 'ABCDE'
-        options = list(self.alternatives)
-        options.append(self.correct_aswer)
-        if options:
-            shuffle(options)
-            for i in range(len(options)):
-                print(f'{letters[i]}) {options[i]}')
+    def check_answer(self, answer: str) -> bool:
+        return answer == self.correct_alternative
