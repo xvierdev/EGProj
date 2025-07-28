539e854ff3b12c9d07cc9d77120d8af57bd391a6
Wesley Xavier <wesley.xvier@gmail.com>
2025-07-28 17:22:36 -0300

    :sparkles: feat: mensagem amigável de fechamento do programa.

--- src/utils/terminal.py
+++ src/utils/terminal.py
@@ -1,4 +1,7 @@
 from os import system, name
+from time import sleep
+
+_SLEEPTIME = 0.03
 
 
 def clear():
@@ -7,3 +10,14 @@ def clear():
     ou 'clear' (Linux/Mac).
     """
     system('cls' if name == 'nt' else 'clear')
+
+
+def quit():
+    """
+    Encerra o programa com uma mensagem amigável ao usuário.
+    """
+    exit_msg = '\nThanks for using the English App! See you next time.\n'
+    for letter in exit_msg:
+        print(letter, end='')
+        sleep(_SLEEPTIME)
+    exit()
