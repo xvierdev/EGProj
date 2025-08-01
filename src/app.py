b16112f4240ceaa42092468227a2f573f5dbbb2a
Wesley Xavier <wesley.xvier@gmail.com>
2025-08-01 13:48:36 -0300

    :bug: fix: update app.py

--- src/app.py
+++ src/app.py
@@ -1,13 +1,14 @@
+from pathlib import Path
+import sqlite3
 from ui import cli_initial_menu, cli_main_menu
-import logging
-
-logging.basicConfig(
-    # references https://docs.python.org/3/howto/logging.html
-    filename='debug.log',
-    format='%(asctime)s - %(levelname)s - %(filename)s - %(message)s',
-    level=logging.DEBUG,
-    encoding='utf-8'
+from connection_factory.create_tables import (
+    create_tables_by_script,
+    get_sql_script_content
 )
+
+
+_SCRIPT = (Path(__file__).parent / 'scripts' / 'main_tables.sql').resolve()
+
 
 funcs = {
     '0': 'Manage Account',
@@ -26,9 +27,13 @@ def main():
     """
     Ponto de entrada da aplicação, função principal de chamada dos menus.
     """
-    logging.info('aplicação inciada.')
-    current_user = cli_initial_menu.user_menu()
-    cli_main_menu.main_menu(current_user, funcs)
+    try:
+        script_path = get_sql_script_content(_SCRIPT)
+        create_tables_by_script(script_path)
+        current_user = cli_initial_menu.user_menu()
+        cli_main_menu.main_menu(current_user, funcs)
+    except (FileNotFoundError, sqlite3.Error) as e:
+        print(f'Erro ao criar tabelas: {e}')
 
 
 if __name__ == '__main__':
