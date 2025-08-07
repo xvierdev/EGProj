e7f2f50778ef237e8d9a1570b52c8c5700c4e7d4
cubo3d <davisilvaperes3@gmail.com>
2025-08-06 22:34:38 -0300

    adicionado frases do presente pro passado(não terminado)

---
+++ src/modules/phrases/past.py
@@ -0,0 +1,74 @@
+def past():
+
+    print(r'''
+        ____                     _____            __
+       / __ \____ ___________   / ___/___  ____  / /____  ____  ________  _____
+      / /_/ / __ `/ ___/ ___/   \__ \/ _ \/ __ \/ __/ _ \/ __ \/ ___/ _ \/ ___/
+     / ____/ /_/ (__  |__  )   ___/ /  __/ / / / /_/  __/ / / / /__/  __(__  )
+    /_/    \__,_/____/____/   /____/\___/_/ /_/\__/\___/_/ /_/\___/\___/____/
+    ''')
+
+    print("Nova disciplina EM DESENVOLVIMENTO")
+
+    print("escreva as palavras a seguir para o passado")
+    print("exemplos:")
+    print("No futuro:")
+    print("I like math")
+    print("No passado:")
+    print("I liked math")
+
+    print("Vamos começar")
+
+    print("primeira frase:")
+    print("João needs to go home")
+
+    print("Qual é a opção correta?")
+
+    print("1. João needed to go home")
+    print("2. João needs to go home")
+    print("3. Nenhuma das duas")
+
+    if input("R:") == '1':
+        print("Resposta correta!")
+    else:
+        print("Resposta errada!")
+
+    print("segunda frase:")
+    print("Do you have any books?")
+
+    print("Qual está correta?")
+
+    print("1. Não tem")
+    print("2. Will you have any books?")
+    print("3. Did you have any books?")
+
+    if input("R:") == '3':
+        print("Resposta correta!")
+    else:
+        print("Resposta errada!")
+
+    print("terceira frase")
+    print("Do you want to go to the store?")
+
+    print("Qual está correta?")
+    print("1. You do not want to go to the store")
+    print("2. You wanted to go to the store?")
+    print("3. Nenhuma")
+
+    if input("R:") == '2':
+        print("Resposta correta!")
+    else:
+        print("Resposta errada!")
+
+    print("quarta frase")
+    print('Matthew does not want to study English')
+
+    print('Qual está correta?')
+    print('1. nenhum')
+    print('2. Matthew would not want to study English')
+    print('3. Would not Matthew like to study English?')
+
+    if input("R:") == '1':
+        print("Resposta correta!")
+    else:
+        print("Resposta errada!")
