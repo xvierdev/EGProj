6d774f5fe345bae6bb90009606dbae2e8b06a522
Wesley Xavier <wesley.xvier@gmail.com>
2025-08-08 16:28:32 -0300

    :wastebasket: remove: remoção de arquivos não utilizados

--- src/static/js/scripts.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const message = document.createElement('p');
-    message.textContent = 'Hello, world! This is a simple JS script.';
-    document.body.appendChild(message);
-});
