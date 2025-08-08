5070e411cc11cfea68761ed7ae1de698e66e559b
Wesley Xavier <wesley.xvier@gmail.com>
2025-08-08 16:30:00 -0300

    :wrench: chore: atualização de script de limpeza.

--- src/scripts/clean.bat
+++ src/scripts/clean.bat
@@ -12,20 +12,19 @@ echo Verificando se estamos dentro da pasta "src"...
 for %%I in (.) do set "current_dir_name=%%~nI"
 
 if /i not "%current_dir_name%"=="src" (
-    echo ERRO: O script nao conseguiu se mover para a pasta "src".
-    pause
-    exit /b 1
+  echo ERRO: O script nao conseguiu se mover para a pasta "src".
+  pause
+  exit /b 1
 )
 
 echo Script executado a partir da pasta "src".
 echo Deletando pastas __pycache__ recursivamente...
 
 for /d /r . %%d in (__pycache__) do (
-    if exist "%%d" (
-        echo Deletando: "%%d"
-        rd /s /q "%%d"
-    )
+  if exist "%%d" (
+    echo Deletando: "%%d"
+    rd /s /q "%%d"
+  )
 )
 
-pause
 endlocal
